006a79e00331f32760163ab9eb0879d76470d41a
LeoTrain <leonardo.bertonasco01@gmail.com>
2023-11-15 11:57:39 +0100

    ex08 finished

--- C08/ex04/ft_stock_str.h
+++ C08/ex04/ft_stock_str.h
@@ -6,12 +6,14 @@
 /*   By: lbertona <lbertona@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 17:17:10 by lbertona          #+#    #+#             */
-/*   Updated: 2023/11/13 17:19:00 by lbertona         ###   ########.fr       */
+/*   Updated: 2023/11/15 11:57:06 by lbertona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STOCK_STR_H
 #define FT_STOCK_STR_H
+
+#include <stdlib.h>
 
 typedef struct s_stock_str
 {
