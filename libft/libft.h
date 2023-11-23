b125d3288c6f317a575192bc5da7d5214beeb61a
LeoTrain <leonardo.bertonasco01@gmail.com>
2023-11-23 13:28:01 +0100

    strlcpy not finished

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lbertona <lbertona@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:04:17 by lbertona          #+#    #+#             */
-/*   Updated: 2023/11/23 11:33:16 by lbertona         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:13:05 by lbertona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ void	*ft_memset(char *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(
+			char *restrict dst, const char *restrict src, size_t dstsize);
 
 #endif
