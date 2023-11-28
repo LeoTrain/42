0366950d8c85ef8d2068102389b8c4b1491d7c23
LeoTrain <leonardo.bertonasco01@gmail.com>
2023-11-28 21:06:21 +0100

    toupper

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lbertona <lbertona@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:04:17 by lbertona          #+#    #+#             */
-/*   Updated: 2023/11/28 19:06:23 by lbertona         ###   ########.fr       */
+/*   Updated: 2023/11/28 21:04:08 by lbertona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(
 			char *restrict dst, const char *restrict src, size_t dstsize);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlcat(
+			char *restrict dst, const char *restrict src, size_t dstsize);
+int		ft_toupper(int c);
 
 #endif
