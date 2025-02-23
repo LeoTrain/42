0115bffae3b7d78819d52b4bdc761504149c03ea
FT_Leo <FT_Leo@42.fr>
2025-02-23 11:17:07 +0100

    added C05

---
+++ piscine_feb_25/C05/first_try/ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leberton <leberton@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 07:13:39 by leberton          #+#    #+#             */
+/*   Updated: 2025/02/10 12:56:30 by leberton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
+	result = nb;
+	while (power > 1)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
